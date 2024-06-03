346c0f1b6463e66941a61ae39efdf330f4652a4e
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-06-03 11:52:59 +0530

    use global window object.

--- src/App.jsx
+++ src/App.jsx
@@ -9,32 +9,9 @@ function App() {
     if (loaded) {
       // Ensure the external script is loaded and PrimoPreviewHandler is available on the window object
       if (window.PrimoPreviewHandler) {
-        const params = {
-          creativeID: 'v3ad-77b1-b6ac-81c6-029d3' // example creative ID
-        };
         
-        window.PrimoPreviewHandler.getHandler(params).then(handler => {
-          console.log('Handler obtained:', handler);
-
-          // Example usage of the handler
-          handler.setupPlayer(
-            document.getElementById('placementDiv'),
-            document.getElementById('mainDiv'),
-            document.getElementById('trackDiv'),
-            'PHONE',
-            {}
-          ).then(player => {
-            console.log('Player setup complete:', player);
-          }).catch(error => {
-            console.error('Error setting up player:', error);
-          });
-
-        }).catch(error => {
-          console.error('Error getting handler:', error);
-        });
-      } else {
-        console.error('PrimoPreviewHandler is not defined');
-      }
+        console.log(PrimoPreviewHandler);
+      } 
     }
   }, [loaded]);
 
