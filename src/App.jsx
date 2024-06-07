f6e482d69094fe1d87eb69fcc99e707d488fac92
Rinkal-2024 <rinkal.dhankariya@iflair.com>
2024-06-07 16:20:15 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -85,13 +85,14 @@ const App = () => {
       }
     });
   });
+
   return (
     <>
-    
       <div className="Header">
         <div className="header-text">
         <b className="text ">PREVIEW</b>
         <b className="text ms-2">Scroll2Animate 300x250 Inline</b></div>
+
         <div className="header-icon">
         <span>
         <img className="me-1" src="https://primo-app.vercel.app/static/media/mobiledefault.ba6ea1d67c509efd9b8e7da634edfe25.svg" id="myImg"></img>
@@ -151,11 +152,27 @@ const App = () => {
 
         <div className="box-4" id="box4"></div>
       </div>
+    
+      </div>
+      <div className="qr">
+      <div className="accordion-item">
+      <h2 className="accordion-header" id="flush-headingOne">
+      <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="true" aria-controls="flush-collapseOne">
+      <div className="qr_info">
+      <span className="qr_title">
+      <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="info" className="svg-inline--fa fa-info mx-2" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 192 512">
+      <path fill="currentColor" d="M48 80a48 48 0 1 1 96 0A48 48 0 1 1 48 80zM0 224c0-17.7 14.3-32 32-32H96c17.7 0 32 14.3 32 32V448h32c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H64V256H32c-17.7 0-32-14.3-32-32z"></path></svg>Creative info</span>
+      <span className="device_res"></span>
+      </div>
+      </button>
+      </h2>
       </div>
 
-      <div className="footer  "></div>
 
-    
+      </div>
+
+      
+      <div className="footer"></div>
     </>
   );
 };
